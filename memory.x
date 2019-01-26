3a6cc6f86f313ce52cedeca5f48f6c49b9885ec0
Sergey Matyukevich <geomatsi@gmail.com>
2019-01-26 22:32:44 +0300

    eziclean: init project
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

---
+++ memory.x
@@ -0,0 +1,9 @@
+MEMORY
+{
+	FLASH : ORIGIN = 0x08000000, LENGTH = 128K
+	RAM : ORIGIN = 0x20000000, LENGTH = 16K
+}
+
+_stack_start = ORIGIN(RAM) + LENGTH(RAM);
+_stext = ORIGIN(FLASH) + 0x400;
+_heap_size = 1024;
---
+++ src/bin/test1.rs
@@ -0,0 +1,31 @@
+#![no_main]
+#![no_std]
+
+extern crate cortex_m_rt as rt;
+use rt::entry;
+use rt::exception;
+use rt::ExceptionFrame;
+
+extern crate cortex_m as cm;
+
+extern crate cortex_m_semihosting as sh;
+use sh::hprintln;
+
+extern crate panic_semihosting;
+
+#[entry]
+fn main() -> ! {
+    loop {
+        hprintln!("Hello World!").unwrap();
+    }
+}
+
+#[exception]
+fn HardFault(ef: &ExceptionFrame) -> ! {
+    panic!("HardFault at {:#?}", ef);
+}
+
+#[exception]
+fn DefaultHandler(irqn: i16) {
+    panic!("Unhandled exception (IRQn = {})", irqn);
+}
