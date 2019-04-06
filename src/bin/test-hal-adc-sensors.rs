b788038951e32103055598c61661c9352bab5f07
Sergey Matyukevich <geomatsi@gmail.com>
2019-04-06 22:52:30 +0300

    adc hal example: use ITM for logging and print all IR sensors
    Switch to ITM for logging. ITM is blazingly fast in comparison with
    semihosting. So it is possible to dump all the sensors in order
    to test their behavior.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- src/bin/test-hal-adc-sensors.rs
+++ src/bin/test-hal-adc-sensors.rs
@@ -7,24 +7,25 @@ use rt::exception;
 use rt::ExceptionFrame;
 
 extern crate cortex_m as cm;
-extern crate panic_semihosting;
+use cm::iprintln;
 
-extern crate cortex_m_semihosting;
-use cortex_m_semihosting::hprintln;
+extern crate panic_itm;
 
 use stm32f1xx_hal::{adc::Adc, prelude::*, stm32};
 
 #[entry]
 fn main() -> ! {
-    let p = stm32::Peripherals::take().unwrap();
+    let mut core = cm::Peripherals::take().unwrap();
+    let d = &mut core.ITM.stim[0];
 
+    let p = stm32::Peripherals::take().unwrap();
     let mut flash = p.FLASH.constrain();
     let mut rcc = p.RCC.constrain();
     let clocks = rcc.cfgr.adcclk(2.mhz()).freeze(&mut flash.acr);
 
-    hprintln!("SYSCLK: {} Hz ...", clocks.sysclk().0).unwrap();
-    hprintln!("PCLK2: {} Hz ...", clocks.pclk2().0).unwrap();
-    hprintln!("ADCCLK: {} Hz ...", clocks.adcclk().0).unwrap();
+    iprintln!(d, "SYSCLK: {} Hz ...", clocks.sysclk().0);
+    iprintln!(d, "PCLK2: {} Hz ...", clocks.pclk2().0);
+    iprintln!(d, "ADCCLK: {} Hz ...", clocks.adcclk().0);
 
     let mut gpioa = p.GPIOA.split(&mut rcc.apb2);
     let mut gpiob = p.GPIOB.split(&mut rcc.apb2);
@@ -32,26 +33,26 @@ fn main() -> ! {
     let mut gpiod = p.GPIOD.split(&mut rcc.apb2);
 
     // PA0-PA7 analog input: ADC_IN0 .. ADC_IN7
-    let mut _ch0 = gpioa.pa0.into_analog(&mut gpioa.crl);
-    let mut _ch1 = gpioa.pa1.into_analog(&mut gpioa.crl);
-    let mut _ch2 = gpioa.pa2.into_analog(&mut gpioa.crl);
-    let mut _ch3 = gpioa.pa3.into_analog(&mut gpioa.crl);
-    let mut _ch4 = gpioa.pa4.into_analog(&mut gpioa.crl);
-    let mut _ch5 = gpioa.pa5.into_analog(&mut gpioa.crl);
-    let mut _ch6 = gpioa.pa6.into_analog(&mut gpioa.crl);
-    let mut _ch7 = gpioa.pa7.into_analog(&mut gpioa.crl);
+    let _ch0 = gpioa.pa0.into_analog(&mut gpioa.crl);
+    let _ch1 = gpioa.pa1.into_analog(&mut gpioa.crl);
+    let _ch2 = gpioa.pa2.into_analog(&mut gpioa.crl);
+    let _ch3 = gpioa.pa3.into_analog(&mut gpioa.crl);
+    let ch4 = gpioa.pa4.into_analog(&mut gpioa.crl);
+    let _ch5 = gpioa.pa5.into_analog(&mut gpioa.crl);
+    let ch6 = gpioa.pa6.into_analog(&mut gpioa.crl);
+    let ch7 = gpioa.pa7.into_analog(&mut gpioa.crl);
 
     // PB0-PB1 analog input: ADC_IN8 .. ADC_IN9
-    let mut ch8 = gpiob.pb0.into_analog(&mut gpiob.crl);
-    let mut _ch9 = gpiob.pb1.into_analog(&mut gpiob.crl);
+    let ch8 = gpiob.pb0.into_analog(&mut gpiob.crl);
+    let ch9 = gpiob.pb1.into_analog(&mut gpiob.crl);
 
     // PC0-PC5 analog input: ADC_IN10 .. ADC_IN15
-    let mut _ch10 = gpioc.pc0.into_analog(&mut gpioc.crl);
-    let mut _ch11 = gpioc.pc1.into_analog(&mut gpioc.crl);
-    let mut _ch12 = gpioc.pc2.into_analog(&mut gpioc.crl);
-    let mut _ch13 = gpioc.pc3.into_analog(&mut gpioc.crl);
-    let mut _ch14 = gpioc.pc4.into_analog(&mut gpioc.crl);
-    let mut _ch15 = gpioc.pc5.into_analog(&mut gpioc.crl);
+    let ch10 = gpioc.pc0.into_analog(&mut gpioc.crl);
+    let ch11 = gpioc.pc1.into_analog(&mut gpioc.crl);
+    let _ch12 = gpioc.pc2.into_analog(&mut gpioc.crl);
+    let _ch13 = gpioc.pc3.into_analog(&mut gpioc.crl);
+    let _ch14 = gpioc.pc4.into_analog(&mut gpioc.crl);
+    let ch15 = gpioc.pc5.into_analog(&mut gpioc.crl);
 
     // PC7: GPIO push-pull output: enable IR LEDs of all the front sensors
     let mut front_leds = gpioc.pc7.into_push_pull_output(&mut gpioc.crl);
@@ -63,16 +64,45 @@ fn main() -> ! {
 
     // ADC setup
     let mut adc = Adc::adc1(p.ADC1, &mut rcc.apb2);
-    let vmax: u16 = adc.max_sample();
+    let max_range: u16 = adc.max_sample();
 
-    hprintln!("Start ADC readings...").unwrap();
+    let mut front_left_90 = ch10;
+    let mut front_left_45 = ch4;
+    let mut front_center = ch6;
+    let mut front_right_45 = ch15;
+    let mut front_right_90 = ch8;
+
+    let mut bottom_left = ch11;
+    let mut bottom_center = ch7;
+    let mut bottom_right = ch9;
+
+    iprintln!(d, "Start ADC readings...");
 
     loop {
-        let data: u16 = adc.read(&mut ch8).unwrap();
+        //let data: u16 = adc.read(&mut ch8).unwrap();
 
-        if data < vmax - 200 {
-            hprintln!("data: {}", data).unwrap();
-        }
+        let sll: u16 = adc.read(&mut front_left_90).unwrap();
+        let slc: u16 = adc.read(&mut front_left_45).unwrap();
+        let scc: u16 = adc.read(&mut front_center).unwrap();
+        let src: u16 = adc.read(&mut front_right_45).unwrap();
+        let srr: u16 = adc.read(&mut front_right_90).unwrap();
+
+        let sl: u16 = adc.read(&mut bottom_left).unwrap();
+        let sc: u16 = adc.read(&mut bottom_center).unwrap();
+        let sr: u16 = adc.read(&mut bottom_right).unwrap();
+
+        iprintln!(
+            d,
+            "front: ({},{},{},{},{})     bottom: ({},{},{})",
+            max_range - sll,
+            max_range - slc,
+            max_range - scc,
+            max_range - src,
+            max_range - srr,
+            max_range - sl,
+            max_range - sc,
+            max_range - sr
+        );
 
         delay(100);
     }
