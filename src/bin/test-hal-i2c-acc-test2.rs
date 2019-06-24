ffe5960a5d62160e0c3b124889dba61d36229007
Sergey Matyukevich <geomatsi@gmail.com>
2019-06-24 22:34:21 +0300

    acc example #2: reset wakeup interrupt in IRQ handler
    Make acc global variable and use it in EXTI interrupt
    handler to reset kxcj9 wakeup interrupt.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- src/bin/test-hal-i2c-acc-test2.rs
+++ src/bin/test-hal-i2c-acc-test2.rs
@@ -16,16 +16,22 @@ extern crate lazy_static;
 use lazy_static::lazy_static;
 
 extern crate stm32f1xx_hal as hal;
-use hal::device::TIM3;
+use hal::device::{TIM2, TIM3};
+use hal::gpio::gpiob::PB2;
+use hal::gpio::gpioe::PE7;
+use hal::gpio::{OpenDrain, Output};
 use hal::prelude::*;
 use hal::stm32;
 use hal::stm32::interrupt;
 use hal::timer::Timer;
 
 extern crate bitbang_hal;
+use bitbang_hal::i2c::I2cBB;
 
 extern crate kxcj9;
-use kxcj9::{GScale8, InterruptPinLatching, InterruptPinPolarity, Kxcj9, Resolution, SlaveAddr};
+use kxcj9::ic::G8Device;
+use kxcj9::{GScale8, Kxcj9, Resolution, SlaveAddr};
+use kxcj9::{InterruptPinLatching, InterruptPinPolarity};
 use kxcj9::{WakeUpInterruptConfig, WakeUpOutputDataRate, WakeUpTriggerMotion};
 
 extern crate nb;
@@ -36,11 +42,13 @@ use core::ops::DerefMut;
 
 /* */
 
+type GAccType = Kxcj9<I2cBB<PE7<Output<OpenDrain>>, PB2<Output<OpenDrain>>, Timer<TIM2>>, G8Device>;
+
 lazy_static! {
     static ref G_EXTI: Mutex<RefCell<Option<stm32::EXTI>>> = Mutex::new(RefCell::new(None));
     static ref G_ITM: Mutex<RefCell<Option<stm32::ITM>>> = Mutex::new(RefCell::new(None));
-    static ref G_TMR: Mutex<RefCell<Option<hal::timer::Timer<TIM3>>>> =
-        Mutex::new(RefCell::new(None));
+    static ref G_TMR: Mutex<RefCell<Option<Timer<TIM3>>>> = Mutex::new(RefCell::new(None));
+    static ref G_ACC: Mutex<RefCell<Option<GAccType>>> = Mutex::new(RefCell::new(None));
 }
 
 /* */
@@ -104,8 +112,8 @@ fn main() -> ! {
                 trigger_motion: WakeUpTriggerMotion {
                     x_negative: true,
                     x_positive: true,
-                    y_negative: false,
-                    y_positive: false,
+                    y_negative: true,
+                    y_positive: true,
                     z_negative: true,
                     z_positive: true,
                 },
@@ -119,6 +127,7 @@ fn main() -> ! {
             G_EXTI.borrow(cs).replace(Some(dp.EXTI));
             G_ITM.borrow(cs).replace(Some(cp.ITM));
             G_TMR.borrow(cs).replace(Some(delay));
+            G_ACC.borrow(cs).replace(Some(acc));
         });
     }
 
@@ -165,6 +174,11 @@ fn EXTI9_5() {
         if let Some(ref mut itm) = G_ITM.borrow(cs).borrow_mut().deref_mut() {
             let d = &mut itm.stim[0];
             iprintln!(d, "Motion !");
+        }
+
+        if let Some(ref mut acc) = G_ACC.borrow(cs).borrow_mut().deref_mut() {
+            let info = acc.read_interrupt_info().unwrap();
+            acc.clear_interrupts().unwrap();
         }
 
         if let Some(exti) = G_EXTI.borrow(cs).borrow().as_ref() {
