46dbfcefe8050863493627bfcff0621436b56741
Sergey Matyukevich <geomatsi@gmail.com>
2019-07-16 23:24:00 +0300

    WIP: update API and implement init
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 #![no_std]
 
+use stm32f1xx_hal as hal;
+
+use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::PwmPin;
+
 pub mod motion;
--- src/motion.rs
+++ src/motion.rs
@@ -1,3 +1,29 @@
+use super::*;
+
+// left wheel gear control
+type LeftWheelRevGear = hal::pwm::Pwm<hal::device::TIM4, hal::pwm::C1>;
+type LeftWheelFwdGear = hal::pwm::Pwm<hal::device::TIM4, hal::pwm::C2>;
+
+type LeftGear = (LeftWheelFwdGear, LeftWheelRevGear);
+
+// right wheel gear control
+type RightWheelFwdGear = hal::pwm::Pwm<hal::device::TIM4, hal::pwm::C3>;
+type RightWheelRevGear = hal::pwm::Pwm<hal::device::TIM4, hal::pwm::C4>;
+
+type RightGear = (RightWheelFwdGear, RightWheelRevGear);
+
+// left wheel dir controls
+type LeftWheelFwdControl = hal::gpio::gpiod::PD5<hal::gpio::Output<hal::gpio::OpenDrain>>;
+type LeftWheelRevControl = hal::gpio::gpiod::PD2<hal::gpio::Output<hal::gpio::OpenDrain>>;
+
+type LeftDir = (LeftWheelFwdControl, LeftWheelRevControl);
+
+// right wheel dir controls
+type RightWheelFwdControl = hal::gpio::gpiob::PB11<hal::gpio::Output<hal::gpio::OpenDrain>>;
+type RightWheelRevControl = hal::gpio::gpioe::PE14<hal::gpio::Output<hal::gpio::OpenDrain>>;
+
+type RightDir = (RightWheelFwdControl, RightWheelRevControl);
+
 /// e.ziclean motion
 ///
 /// Pins PB6-PB9 (TIM4 output channels):
@@ -33,65 +59,137 @@
 /// |  1   |   1  |  -  |  -  |
 ///  _________________________
 
-
 /// Motion error
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
 pub enum Error {
     /// HardwareError
     HardwareError,
 }
 
 /// Wheel rotation direction
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
 pub enum Direction {
     Forward,
     Reverse,
 }
 
 /// Vehicle rotation direction
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
 pub enum Rotation {
     Left,
     Right,
 }
 
 /// Gear
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
 pub enum Gear {
+    Neutral,
     Low,
     Medium,
     Top,
 }
 
-/// Motion
-pub struct Motion
-{
-    pwm: u32,
+/// Left Wheel
+struct LeftWheel {
+    dir: Direction,
+    gear: Gear,
+    gc: LeftGear,
+    dc: LeftDir,
 }
 
-impl Motion
-{
-    pub fn init(pwm: u32) -> Self {
-        Motion { pwm }
+/// Right Wheel
+struct RightWheel {
+    dir: Direction,
+    gear: Gear,
+    gc: RightGear,
+    dc: RightDir,
+}
+
+/// Motion
+pub struct Motion {
+    /// Right wheel state
+    right: RightWheel,
+    /// Left wheel state
+    left: LeftWheel,
+}
+
+impl Motion {
+    pub fn init(
+        (lgf, lgr): LeftGear,
+        (rgf, rgr): RightGear,
+        (lcf, lcr): LeftDir,
+        (rcf, rcr): RightDir,
+    ) -> Self {
+        let mut m = Motion {
+            left: LeftWheel {
+                dir: Direction::Forward,
+                gear: Gear::Neutral,
+                gc: (lgf, lgr),
+                dc: (lcf, lcr),
+            },
+            right: RightWheel {
+                dir: Direction::Forward,
+                gear: Gear::Neutral,
+                gc: (rgf, rgr),
+                dc: (rcf, rcr),
+            },
+        };
+
+        m.left.gc.0.disable();
+        m.left.gc.1.disable();
+
+        m.left.dc.0.set_low();
+        m.left.dc.1.set_low();
+
+        m.right.gc.0.disable();
+        m.right.gc.1.disable();
+
+        m.right.dc.0.set_low();
+        m.right.dc.1.set_low();
+
+        m
     }
 
-    pub fn forward(gear: Gear) -> Result<(), Error> {
+    pub fn forward(&mut self, gear: Gear) -> Result<(), Error> {
+        self.set_right_wheel(Direction::Forward, gear)?;
+        self.set_left_wheel(Direction::Forward, gear)?;
+
         Ok(())
     }
 
-    pub fn backwards(gear: Gear) -> Result<(), Error> {
+    pub fn stop(&mut self) -> Result<(), Error> {
+        self.forward(Gear::Neutral)?;
+
         Ok(())
     }
 
-    pub fn rotate(rot: Rotation, gear: Gear) -> Result<(), Error> {
+    pub fn backwards(&mut self, gear: Gear) -> Result<(), Error> {
+        self.set_right_wheel(Direction::Reverse, gear)?;
+        self.set_left_wheel(Direction::Reverse, gear)?;
+
         Ok(())
     }
 
-    pub fn set_left_wheel(dir: Direction, gear: Gear) -> Result<(), Error> {
+    pub fn rotate(&mut self, rot: Rotation, gear: Gear) -> Result<(), Error> {
+        match rot {
+            Rotation::Left => {
+                self.set_right_wheel(Direction::Forward, gear)?;
+                self.set_left_wheel(Direction::Reverse, gear)?;
+            }
+            Rotation::Right => {
+                self.set_right_wheel(Direction::Reverse, gear)?;
+                self.set_left_wheel(Direction::Forward, gear)?;
+            }
+        }
+
         Ok(())
     }
 
-    pub fn set_right_wheel(dir: Direction, gear: Gear) -> Result<(), Error> {
+    pub fn set_left_wheel(&mut self, dir: Direction, gear: Gear) -> Result<(), Error> {
+        Ok(())
+    }
+
+    pub fn set_right_wheel(&mut self, dir: Direction, gear: Gear) -> Result<(), Error> {
         Ok(())
     }
 }
