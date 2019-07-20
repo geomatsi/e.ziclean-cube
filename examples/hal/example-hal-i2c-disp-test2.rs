09320778158a0af8473c34775756e37511eb9a99
Sergey Matyukevich <geomatsi@gmail.com>
2019-07-20 13:29:28 +0300

    example: hal: display example #2
    Add simple display example that uses eziclean::display library.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

---
+++ examples/hal/example-hal-i2c-disp-test2.rs
@@ -0,0 +1,71 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt as rt;
+use rt::entry;
+
+use cm::iprintln;
+use cortex_m as cm;
+
+use eziclean::display;
+use nb::block;
+use panic_itm as _;
+
+use stm32f1xx_hal::timer::Timer;
+use stm32f1xx_hal::{prelude::*, stm32};
+
+#[entry]
+fn main() -> ! {
+    let mut core = cm::Peripherals::take().unwrap();
+    let dbg = &mut core.ITM.stim[0];
+
+    let p = stm32::Peripherals::take().unwrap();
+    let mut rcc = p.RCC.constrain();
+    let mut flash = p.FLASH.constrain();
+
+    let clocks = rcc
+        .cfgr
+        .sysclk(8.mhz())
+        .pclk1(8.mhz())
+        .freeze(&mut flash.acr);
+
+    let mut gpioa = p.GPIOA.split(&mut rcc.apb2);
+    let mut gpioc = p.GPIOC.split(&mut rcc.apb2);
+    let mut gpiod = p.GPIOD.split(&mut rcc.apb2);
+    let mut gpioe = p.GPIOE.split(&mut rcc.apb2);
+
+    let tmp = gpioe.pe15.into_floating_input(&mut gpioe.crh);
+    let clk = gpioc.pc8.into_push_pull_output(&mut gpioc.crh);
+    let dio = gpiod.pd14.into_push_pull_output(&mut gpiod.crh);
+    let stb = gpioa.pa11.into_push_pull_output(&mut gpioa.crh);
+
+    let timer = Timer::tim2(p.TIM2, 200.khz(), clocks, &mut rcc.apb1);
+    let mut delay = Timer::tim3(p.TIM3, 2.hz(), clocks, &mut rcc.apb1);
+
+    let mut screen = display::Display::init(stb, dio, clk, tmp, timer);
+
+    loop {
+        iprintln!(dbg, "clean");
+        screen.clear();
+        block!(delay.wait()).ok();
+
+        for i in 0..10 {
+            screen.print4([i, i, i, i]).unwrap();
+            block!(delay.wait()).ok();
+        }
+
+        for i in 0..10 {
+            for p in 0..4 {
+                screen.print(p, i).unwrap();
+                block!(delay.wait()).ok();
+            }
+        }
+
+        for i in 10..20 {
+            for k in [true, false, true, false].iter() {
+                screen.print_time([i, 30 - i], *k).unwrap();
+                block!(delay.wait()).ok();
+            }
+        }
+    }
+}
