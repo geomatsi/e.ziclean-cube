ac5e00f6c19f74ae19337ef9ff28a837bd60d60a
Sergey Matyukevich <geomatsi@gmail.com>
2019-09-29 19:46:27 +0300

    examples: one more timer example
    Try to measure one TIM using another one. This example helps to understand
    how much we can do in TIM interrupts. For instance, if TIM interrupt is
    called too often in comparison with sysclk, then it is not able to cope
    with all the instructions that it is supposed to do.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

---
+++ examples/hal/example-hal-timer-irq-test2.rs
@@ -0,0 +1,132 @@
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+
+use cm::interrupt::Mutex;
+use cm::iprintln;
+use cm::singleton;
+use cortex_m as cm;
+
+use panic_itm as _;
+
+use hal::prelude::*;
+use hal::stm32;
+use hal::stm32::interrupt;
+use hal::timer::CountDownTimer;
+use hal::timer::Event;
+use hal::timer::Timer;
+use stm32f1xx_hal as hal;
+
+use core::cell::RefCell;
+use core::ops::DerefMut;
+
+type DbgPort = stm32::ITM;
+type ReadBuf = &'static mut [u32; 10];
+
+const FREQ: u32 = 40_000;
+const TEST_FREQ: u32 = 1_000;
+
+static G_TIM2: Mutex<RefCell<Option<CountDownTimer<stm32::TIM2>>>> = Mutex::new(RefCell::new(None));
+static G_TIM3: Mutex<RefCell<Option<CountDownTimer<stm32::TIM3>>>> = Mutex::new(RefCell::new(None));
+static G_ITM: Mutex<RefCell<Option<DbgPort>>> = Mutex::new(RefCell::new(None));
+static G_CNT: Mutex<RefCell<Option<u32>>> = Mutex::new(RefCell::new(None));
+static G_BUF: Mutex<RefCell<Option<ReadBuf>>> = Mutex::new(RefCell::new(None));
+static G_IDX: Mutex<RefCell<Option<usize>>> = Mutex::new(RefCell::new(None));
+
+#[entry]
+fn main() -> ! {
+    if let (Some(mut cp), Some(dp)) = (cm::Peripherals::take(), stm32::Peripherals::take()) {
+        cm::interrupt::free(|cs| {
+            let mut rcc = dp.RCC.constrain();
+            let mut flash = dp.FLASH.constrain();
+
+            let clocks = rcc
+                .cfgr
+                .sysclk(8.mhz())
+                .pclk1(8.mhz())
+                .freeze(&mut flash.acr);
+
+            let buf = singleton!(: [u32; 10] = [0; 10]).unwrap();
+
+            let mut tim2 = Timer::tim2(dp.TIM2, &clocks, &mut rcc.apb1).start_count_down(FREQ.hz());
+            tim2.listen(Event::Update);
+
+            let mut tim3 =
+                Timer::tim3(dp.TIM3, &clocks, &mut rcc.apb1).start_count_down(TEST_FREQ.hz());
+            tim3.listen(Event::Update);
+
+            setup_interrupts(&mut cp);
+
+            G_ITM.borrow(cs).replace(Some(cp.ITM));
+            G_TIM2.borrow(cs).replace(Some(tim2));
+            G_TIM3.borrow(cs).replace(Some(tim3));
+            G_BUF.borrow(cs).replace(Some(buf));
+            G_CNT.borrow(cs).replace(Some(0));
+            G_IDX.borrow(cs).replace(Some(0));
+        });
+    }
+
+    loop {
+        cm::asm::nop();
+    }
+}
+
+fn setup_interrupts(cp: &mut cm::peripheral::Peripherals) {
+    let nvic = &mut cp.NVIC;
+
+    unsafe {
+        cm::peripheral::NVIC::unmask(stm32::Interrupt::TIM2);
+        cm::peripheral::NVIC::unmask(stm32::Interrupt::TIM3);
+
+        nvic.set_priority(stm32::Interrupt::TIM2, 1);
+        nvic.set_priority(stm32::Interrupt::TIM3, 1);
+    }
+
+    cm::peripheral::NVIC::unpend(stm32::Interrupt::TIM2);
+    cm::peripheral::NVIC::unpend(stm32::Interrupt::TIM3);
+}
+
+#[interrupt]
+fn TIM2() {
+    cm::interrupt::free(|cs| {
+        if let (Some(ref mut tim), Some(ref mut cnt)) = (
+            G_TIM2.borrow(cs).borrow_mut().deref_mut(),
+            G_CNT.borrow(cs).borrow_mut().deref_mut(),
+        ) {
+            *cnt = cnt.wrapping_add(1);
+            tim.clear_update_interrupt_flag();
+        }
+    });
+}
+
+#[interrupt]
+fn TIM3() {
+    cm::interrupt::free(|cs| {
+        if let (
+            Some(ref mut tim),
+            Some(ref mut itm),
+            Some(ref mut cnt),
+            Some(ref mut idx),
+            Some(ref mut buf),
+        ) = (
+            G_TIM3.borrow(cs).borrow_mut().deref_mut(),
+            G_ITM.borrow(cs).borrow_mut().deref_mut(),
+            G_CNT.borrow(cs).borrow_mut().deref_mut(),
+            G_IDX.borrow(cs).borrow_mut().deref_mut(),
+            G_BUF.borrow(cs).borrow_mut().deref_mut(),
+        ) {
+            let _d = &mut itm.stim[0];
+
+            if *idx < buf.len() {
+                buf[*idx] = *cnt;
+                *idx = *idx + 1;
+            } else {
+                iprintln!(_d, "ts: {:?}", buf);
+                *idx = 0;
+            }
+
+            tim.clear_update_interrupt_flag();
+        }
+    });
+}
